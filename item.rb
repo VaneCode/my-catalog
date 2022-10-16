e1a3aa360b78e39ded23221543173c131ecaf036
vanessaBonito <vanebonito@hotmail.com>
2022-10-16 18:46:22 -0500

    Add getters for relations properties

--- item.rb
+++ item.rb
@@ -1,10 +1,27 @@
 require 'securerandom'
 class Item
-    attr_reader :id, :archived, :genre, :author, :source, :label,
-    attr_accessor :publish_date
-    def initialize(id: ,publish_date:, archived:)
-        @id = SecureRandom.random_number(1000)
-        @publish_date = publish_date
-    end
-    
+  attr_reader :id, :genre, :author, :source, :label, :archived
+  attr_accessor :publish_date
+
+  def initialize(id:, publish_date:, archived:)
+    @id = SecureRandom.random_number(1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
 end
